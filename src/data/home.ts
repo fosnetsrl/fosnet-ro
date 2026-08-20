ab9dfce890a5ee75f958c1619bca29a40eefb61a
fosnetsrl <fosnetsrl@gmail.com>
2026-08-20 22:08:31 +0300

    Update home.ts

--- src/data/home.ts
+++ src/data/home.ts
@@ -2,6 +2,6 @@ export const links = [
   { href: "/", label: "Acasă" },
   { href: "/services/", label: "Servicii" },
   { href: "/about/", label: "Despre" },
-  { href: "/portfolio/", label: "Portofoliu" },
+  // { href: "/portfolio/", label: "Portofoliu" },
   { href: "/contact/", label: "Contact" },
 ];
